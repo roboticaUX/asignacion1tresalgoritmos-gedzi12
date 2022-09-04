aced9b25cd3d15b0a7686d801a1ade6365a0893f
Erick Melgar <97333015+ErickMelgar@users.noreply.github.com>
2022-09-04 15:54:36 -0500

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n== 0 or n ==1:
-        return 1
-    else:
-        return n*factorial(n-1)
-
-num= int(input('ingresar dato: '))
-fact=factorial(num)
-print('Resultado: ', fact)
